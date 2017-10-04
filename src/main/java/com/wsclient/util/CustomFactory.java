5b1c6fc5f763aade014e989a166d0439bffd089e
wspilot16 <wspilot16@gmail.com>
2017-10-04 16:47:28 +0530

    added component annotation

--- src/main/java/com/wsclient/util/CustomFactory.java
+++ src/main/java/com/wsclient/util/CustomFactory.java
@@ -12,8 +12,10 @@ import org.apache.http.conn.ssl.TrustStrategy;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+@Component
 public class CustomFactory {
 
 	public RestTemplate getRestTemplate() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException {

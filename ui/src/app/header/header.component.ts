b3f5cedda28ae7b42e3b51b0a9b2ad861a322fb1
dileep <adidamdileep@gmail.com>
2018-01-03 13:33:54 +0530

    changes regarding focus and css

--- ui/src/app/header/header.component.ts
+++ ui/src/app/header/header.component.ts
@@ -60,6 +60,8 @@ export class TabComponent implements OnInit {
         }
       }
       $('#overlay').fadeOut();
+      window.setTimeout(function(){ $('#response-label')[0].scrollIntoView(!0);},1000);
+     
     });
   }
   }

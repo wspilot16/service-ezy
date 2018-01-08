a43d8021e1138645f94d24afc490303dee6059d7
prasengupta <pratik006@gmail.com>
2018-01-08 21:00:30 +0530

    minor pom changes

--- service/src/main/java/demo/Application.java
+++ service/src/main/java/demo/Application.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Configuration;
 @EnableAutoConfiguration
 @ComponentScan(basePackages = { "com.wsclient" })
 public class Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
